1d1ed5756e747b4b08c9f26fc523335ba5599b62
gaozhidong <1011284295@qq.com>
2017-04-20 13:46:42 +0800

    更新样式

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
   constructor(props) {
@@ -7,25 +9,32 @@ class App extends Component {
     this.state = {
       newTodo: 'test',
       todoList: [
-        { id: 1, title: '第一个代办' }
+        { id: 1, title: '第一个待办' },
+        { id: 2, title: '第二个待办' },
       ]
     }
   }
   render() {
+
     let todos = this.state.todoList.map((item, index) => {
-      return <li>{item.title}</li>
+      return ( // 为什么这里要加个括号？这是动手题3 🐸
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
     })
+
     return (
       <div className="App">
-        <h1>我的代办</h1>
+        <h1>我的待办</h1>
         <div className="inputWrapper">
-          <input type="text" value={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
           {todos}
         </ol>
       </div>
-    );
+    )
   }
 }
 
