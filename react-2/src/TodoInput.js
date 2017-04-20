27aecabadc908521e3f67569c6cb095d2dd537f0
gaozhidong <1011284295@qq.com>
2017-04-20 14:07:59 +0800

    增删改查

--- react-2/src/TodoInput.js
+++ react-2/src/TodoInput.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import './TodoInput.css'
 
 export default class TodoInput extends Component {
     render() {
         return <input type="text" value={this.props.content}
+            className="TodoInput"
             onChange={this.changeTitle.bind(this)}
             onKeyPress={this.submit.bind(this)} />
     }
@@ -15,4 +17,3 @@ export default class TodoInput extends Component {
         this.props.onChange(e)
     }
 }
-
