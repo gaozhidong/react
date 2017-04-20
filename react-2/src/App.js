b2c391fa98e32bbd6b5c00fc834e415b53b80635
gaozhidong <1011284295@qq.com>
2017-04-20 13:57:29 +0800

    可以新增 todo 了，但是 newTodo 却不会重置

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import 'normalize.css'
+import './reset.css'
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
@@ -7,10 +9,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      newTodo: 'test',
+      newTodo: '',
       todoList: [
-        { id: 1, title: '第一个待办' },
-        { id: 2, title: '第二个待办' },
       ]
     }
   }
@@ -18,7 +18,7 @@ class App extends Component {
 
     let todos = this.state.todoList.map((item, index) => {
       return ( // 为什么这里要加个括号？这是动手题3 🐸
-        <li>
+        <li key={index} >
           <TodoItem todo={item} />
         </li>
       )
@@ -28,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
@@ -36,6 +36,25 @@ class App extends Component {
       </div>
     )
   }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;
+
+let id = 0
+
+function idMaker() {
+  id += 1
+  return id
+}
--- react-2/src/TodoInput.js
+++ react-2/src/TodoInput.js
@@ -1,7 +1,12 @@
-import React,{Component} from 'react';
-
-export default class TodoInput extends Component{
-    render(){
-        return <input type="text" value={this.props.content} />        
+import React, { Component } from 'react';
+export default class TodoInput extends Component {
+    render() {
+        return <input type="text" defaultValue={this.props.content}
+            onKeyPress={this.submit.bind(this)} />
+    }
+    submit(e) {
+        if (e.key === 'Enter') {
+           this.props.onSubmit(e)
+        }
     }
 }
