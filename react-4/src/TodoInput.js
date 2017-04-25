fcebcf7bd648cd19e72ca69df0f533f28be687cf
gaozhidong <1011284295@qq.com>
2017-04-25 10:37:40 +0800

    add placeholder

--- react-4/src/TodoInput.js
+++ react-4/src/TodoInput.js
@@ -1,8 +1,9 @@
 import React,{ Component } from 'react';
 
+
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" value={this.props.content}
+        return <input type="text" value={this.props.content} placeholder="输入代办事项"
         className="TodoInput"
         onChange={this.changeTitle.bind(this)}
         onKeyPress={this.submit.bind(this)}/>
