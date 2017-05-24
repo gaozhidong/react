9a83d912f35e3949e8a49e96759f10ca079bd3d0
gaozhidong <1011284295@qq.com>
2017-05-24 18:07:34 +0800

    signInOrSignUp 抽离成一个组件

--- react-5/src/login.js
+++ react-5/src/login.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import './login.css';
 import { signUp, signIn, sendPasswordResetEmail } from './leanCloud'
 import errorInfo from './error'
-import SignUpForm from './SignUpForm' //将 SignUpForm 抽离成一个组件
-import SignInForm from './SignInForm' //将SignInForm 抽离成一个组件
+import SignInOrSignUp from './SignInOrSignUp' //登录注册合成一个组件
 import ForgotPasswordForm from './ForgotPasswordForm' //将忘记密码表单抽离成一个组件
 
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            selected: 'signUp',
             selectedTsb: 'signInOrSignUp',
             formData: {
                 email: '',
@@ -19,11 +17,7 @@ class Login extends Component {
             }
         }
     }
-    switch(e) {
-        this.setState({
-            selected: e.target.value
-        })
-    }
+
     signUp(e) {
         if (!this.state.formData.username || !this.state.formData.password) {
             alert('请输入用户名或密码')
@@ -65,52 +59,26 @@ class Login extends Component {
     }
     render() {
 
-        let signInOrSignUp = (
-            <div className="signInOrSignUp">
-                <nav>
-                    <label>
-                        <input type="radio" value="signUp"
-                            checked={this.state.selected === 'signUp'}
-                            onChange={this.switch.bind(this)}
-                        /> 注册</label>
-                    <label>
-                        <input type="radio" value="signIn"
-                            checked={this.state.selected === 'signIn'}
-                            onChange={this.switch.bind(this)}
-                        /> 登录</label>
-                </nav>
-                <div className="panes">
-
-                    {this.state.selected === 'signUp' ?
-                        <SignUpForm formData={this.state.formData}
-                            onSubmit={this.signUp.bind(this)}
-                            onChange={this.changeFormData.bind(this)}
-                        />
-                        : null}
-                    {this.state.selected === 'signIn' ?
-                        <SignInForm formData={this.state.formData}
-                            onChange={this.changeFormData.bind(this)}
-                            onSubmit={this.signIn.bind(this)}
-                            onForgotPassword={this.showForgotPassword.bind(this)}
-                        />
-                        : null}
-
-                </div>
-            </div>
-        )
-
         return (
             <div className="login">
                 <div className="UserDialog">
-                    {this.state.selectedTab === 'signInOrSignUp' ?
-                        signInOrSignUp :
-                        <ForgotPasswordForm
-                            formData={this.state.formData}
-                            onSubmit={this.resetPassword.bind(this)}
-                            onChange={this.changeFormData.bind(this)}
-                            onSignIn={this.returnToSignIn.bind(this)}
-                        />}
+                    {
+                        this.state.selectedTab === 'signInOrSignUp' ?
+                            <SignInOrSignUp
+                                formData={this.state.formData}
+                                onSignIn={this.signIn.bind(this)}
+                                onSignUp={this.signUp.bind(this)}
+                                onChange={this.changeFormData.bind(this)}
+                                onForgotPassword={this.showForgotPassword.bind(this)}
+                            /> :
+                            <ForgotPasswordForm
+                                formData={this.state.formData}
+                                onSubmit={this.resetPassword.bind(this)}
+                                onChange={this.changeFormData.bind(this)}
+                                onSignIn={this.returnToSignIn.bind(this)}
 
+                            />
+                    }
                 </div>
             </div>
         )
